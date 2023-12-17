eabf28b27736f8ca86d68d0bcaadac8a60df92af
unknown <arsensalkanovic@gmail.com>
2023-12-17 23:55:02 +0100

    bestand tijdelijk.py toegevoegd

---
+++ tijdelijk/prijzen.py
@@ -0,0 +1,10 @@
+mijn_dictionary = {
+  "aardbei" : 3,
+  "vanille" : 4,
+  "chocolade" : 5,
+}
+keys = mijn_dictionary.keys()
+values = mijn_dictionary.values()
+print("keys:", keys)
+print("values:", values)
+   
