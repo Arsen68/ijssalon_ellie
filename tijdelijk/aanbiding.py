16fde7150e827ff93f8bbaf8bc6622ddc91c02c5
unknown <arsensalkanovic@gmail.com>
2024-01-16 20:05:35 +0100

    tijdelik last version

--- tijdelijk/aanbiding.py
+++ tijdelijk/aanbiding.py
@@ -1,25 +1,25 @@
-def print_aanbieding():
-    prijzen = {
-        "aardbei" : 3,
-        "vanille" : 4,
-        "chocolade" : 5
-    }
+prijzen = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+}
 
-    aanbieding = prijzen["aardbei"] * 0.8
+aanbieding = prijzen ["aardbei"] * 0.8
 
-    reclame_tekst = f"Vandaag in de aanbieding: aardbei, 1 liter - slechts € {aanbieding}"
+reclame_tekst = f"Vandaag in de aanbieding: Aardbei, 1 Liter - slechts € {aanbieding}"
+print (reclame_tekst)
+reclame_tekst2 = reclame_tekst[:63]
+print(reclame_tekst2)
+reclame_tekst3 = reclame_tekst2.upper()
+print(reclame_tekst3)
+reclame_tekst4 = reclame_tekst3.split()
+print(reclame_tekst4)
+el = ["Vandaag in de aanbieding:", "Aardbei","Liter","slechts"]
+print (el)
+for el in reclame_tekst4:
+    if len(el) > 4:
+     print (el.upper())    
+    else:
+        print(el.lower())
 
-    reclame_tekst2 = reclame_tekst[:63]
-
-    reclame_tekst3 = reclame_tekst2.upper()
-
-    reclame_tekst4 = reclame_tekst3.split()
-
-    for el in reclame_tekst4:
-        if len(el) > 4:
-            print(el.upper())
-        else:
-            print(el.lower())
-
-    ("Aanbieding")
-print_aanbieding()
+print('"bestand tijdelijk.py gecodeerd"')
